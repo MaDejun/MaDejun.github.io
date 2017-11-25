b1208f805eb4684dacbe3181322238719793f902
MaDejun <1510453254@qq.com>
2017-11-25 22:31:14 +0800

    add photos

--- tool.py
+++ tool.py
@@ -129,7 +129,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("./source/photos/data.json",'utf-8') as fp:
+    with open("./source/photos/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
